f7bed371fa4faa9c37d77201a2301ff7fc7b1345
Raymond Li <Rlii0303@gmail.com>
2020-12-10 17:19:08 -0800

    changed search

--- src/App.js
+++ src/App.js
@@ -1,46 +1,36 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import Input from "./components/input";
 import List from "./components/video-list";
 import Display from "./components/display";
-import useVideos from "../src/customHook/useVideos"
+import useVideos from "../src/customHook/useVideos";
 
-
-
-
-
-
-let App = ()=>{
+let App = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
-  const [videos, onFormSubmit] = useVideos(`Americas got talent`);
-  
-  useEffect(()=>{
+  const [videos, onFormSubmit] = useVideos(`golden buzzer Americas got talent`);
+
+  useEffect(() => {
     setSelectedVideo(videos[0]);
-  },[videos]);
-  
+  }, [videos]);
 
-  let onVideoSelect = (video) => {
+  let onVideoSelect = video => {
     setSelectedVideo(video);
-  }
-
- 
+  };
 
   return (
     <div className="ui container">
-      <Input onFormSubmit={onFormSubmit}/>
-      <div className = "ui grid">
-        <div className = "ui row">
-          <div className = "eleven wide column">
-            <Display selected = {selectedVideo}/> 
+      <Input onFormSubmit={onFormSubmit} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <Display selected={selectedVideo} />
           </div>
-          <div className = "five wide column">
-            <List  onVideoSelect = {onVideoSelect} videos = {videos}/>
+          <div className="five wide column">
+            <List onVideoSelect={onVideoSelect} videos={videos} />
           </div>
         </div>
       </div>
     </div>
-    );
-}
-
-
+  );
+};
 
 export default App;
